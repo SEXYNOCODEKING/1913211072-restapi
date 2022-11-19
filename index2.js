e5196caced9a79ec1a0d14696969981b99043bbe
Zerdsexy <pr.thanawat_st@tni.ac.th>
2022-11-19 12:04:24 +0700

    second summit

---
+++ index2.js
@@ -0,0 +1,16 @@
+const drink = ["Soda","NOCODE","Tea"]
+
+for (const d of drink){
+    console.log(d)
+}
+
+const users = [
+    {name : 'DINO' ,age:9000},
+    {name : 'POND' ,age:9021},
+    {name : 'NOCODEKING',age:1}
+]
+
+for (const u of users){
+    console.log(`${u.name}
+    ${u.age}`)
+}
